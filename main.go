5e8aec61d3268c69121b0647ffd3d847f7bdbd6f
Lorenzo <lorenzof@themediaoptimizers.com>
2025-01-11 07:35:59 -0300

    First codes

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+	"github.com/go-chi/chi"
+	"net/http"
+	"github.com/go-chi/cors"
+)
+
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Warning: no .env file found")
+	}
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+	log.Printf("server starting on port %v", portString)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
